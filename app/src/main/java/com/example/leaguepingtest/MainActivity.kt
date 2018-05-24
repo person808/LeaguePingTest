9ad7aea74d10753d8c785ffb9fcc4546a74b6ae0
person808 <kainaluh808@gmail.com>
2018-05-23 14:30:12 -1000

    Get rid of button.

--- app/src/main/java/com/example/leaguepingtest/MainActivity.kt
+++ app/src/main/java/com/example/leaguepingtest/MainActivity.kt
@@ -5,11 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.*
-import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
 import org.icmp4j.IcmpPingUtil
 
 val IP_ADDRESSES = mapOf(
@@ -22,32 +19,23 @@ val IP_ADDRESSES = mapOf(
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     val textView: TextView by lazy { findViewById<TextView>(R.id.textView) }
-    val button: Button by lazy { findViewById<Button>(R.id.button) }
     val spinner: Spinner by lazy { findViewById<Spinner>(R.id.spinner) }
     private var ipAddress = IP_ADDRESSES["NA"]!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val adapter = ArrayAdapter.createFromResource(this,
-                R.array.servers, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                R.array.servers, android.R.layout.simple_spinner_item).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
         spinner.adapter = adapter
         spinner.onItemSelectedListener = this
 
-        button.setOnClickListener({ runPingTest() })
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        ipAddress = IP_ADDRESSES[parent?.getItemAtPosition(position)]!!
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-    private fun runPingTest() {
         launch(UI) {
-            while (true) {
+            while (isActive) {
                 val ping = getPing(ipAddress).await()
                 when (ping) {
                     is PingStatus.Success -> {
@@ -59,6 +47,12 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             }
         }
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        ipAddress = IP_ADDRESSES[parent?.getItemAtPosition(position)]!!
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 
     private fun getPing(address: String): Deferred<PingStatus> = async {
         val pingRequest = IcmpPingUtil.createIcmpPingRequest().apply {
