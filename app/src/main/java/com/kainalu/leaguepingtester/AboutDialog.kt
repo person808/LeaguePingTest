8a0dbc31b3b319ac1f5b9fe54fecdaebd80b5da0
person808 <kainaluh808@gmail.com>
2019-10-06 21:41:28 -0700

    Update Material components library.

--- app/src/main/java/com/kainalu/leaguepingtester/AboutDialog.kt
+++ app/src/main/java/com/kainalu/leaguepingtester/AboutDialog.kt
@@ -4,25 +4,66 @@ import android.app.Dialog
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import com.afollestad.materialdialogs.MaterialDialog
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
+import android.text.util.Linkify
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
-class AboutDialog : androidx.fragment.app.DialogFragment() {
+class AboutDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-             val builder = MaterialDialog.Builder(activity as Context)
-                    .title("${getString(R.string.app_name)} ${getVersion(activity as Context)}")
-                    .content(R.string.about_body, true)
-                    .positiveText(R.string.close)
-            return builder.build()
+        val horizontalPadding = resources.getDimensionPixelSize(R.dimen.dialogHorizontalPadding)
+        val verticalPadding = resources.getDimensionPixelSize(R.dimen.dialogVerticalPadding)
+        val message = TextView(requireContext()).apply {
+            setPaddingRelative(horizontalPadding, verticalPadding, horizontalPadding, verticalPadding)
+            linksClickable = true
+            movementMethod = LinkMovementMethod.getInstance()
+            text = linkifyHtml(getString(R.string.about_body), Linkify.WEB_URLS)
+        }
+        R.attr.alertDialogTheme
+        return MaterialAlertDialogBuilder(requireContext())
+                .setTitle("${getString(R.string.app_name)} v${getVersion(requireContext())}")
+                .setView(message)
+                //.setMessage(linkifyHtml(getString(R.string.about_body), Linkify.WEB_URLS))
+                .setPositiveButton(R.string.close) { _, _ -> dismiss() }
+                .create()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        view?.findViewById<TextView>(android.R.id.message)?.apply {
+            linksClickable = true
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+
+    private fun linkifyHtml(html: String, linkifyMask: Int): Spannable {
+        val text = Html.fromHtml(html)
+        val currentSpans = text.getSpans(0, text.length, URLSpan::class.java)
+
+        val buffer = SpannableString(text)
+        Linkify.addLinks(buffer, linkifyMask)
+
+        for (span in currentSpans) {
+            val end = text.getSpanEnd(span)
+            val start = text.getSpanStart(span)
+            buffer.setSpan(span, start, end, 0)
+        }
+        return buffer
     }
 
     companion object {
         fun getVersion(context: Context): String {
-            try {
-                return context.packageManager.getPackageInfo(context.packageName, 0).versionName
+            return try {
+                context.packageManager.getPackageInfo(context.packageName, 0).versionName
             } catch (e: PackageManager.NameNotFoundException) {
-                return "Unknown"
+                "Unknown"
             }
         }
     }
